e6c2d9cf819b9d54168efe1acd44d9b9eb89ed72
namle <lephuongnam958506@gmail.com>
2025-03-07 09:30:09 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,16 @@ import ContactBook from "@/views/ContactBook.vue";
 
 const routes = [
     {
-    path: "/",
-    name: "contactbook",
-    component: ContactBook,
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+
 ];
 
 const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import router from '@/router';
+
+</script>
+
+<template>
+    <div class="page">
+        <p>
+            Oops, Không thể tìm thấy trang, Trở về
+            <router-link to="/">Trang chủ</router-link>
+        </p>
+    </div>
+</template>
